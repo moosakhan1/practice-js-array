b628f1784ec9018ff4850439caab751d48cd574c
moosakhan1 <144359037+moosakhan1@users.noreply.github.com>
2023-11-12 21:43:12 +0500

    created arrray practice

---
+++ app.js
@@ -0,0 +1,48 @@
+// // part 1
+// let a = [];
+// let b =+prompt("enter your number1");
+// let c =+prompt("enter your number2");
+// let d =+prompt("enter your number3");
+// a.push(b)
+// a.push(c)
+// a.push(d)
+// console.log(a)
+
+// // part 2
+
+// {
+//     let arr = [];
+// let i;
+// do {
+//     i = +prompt("enter your number");
+//     arr.push(i);
+//     console.log(arr)
+    
+// }while(i!= 0);
+// console.log(i);
+// }
+
+// // part 3
+
+// let arrr = [1,2,3,4,56,65,10,20,30];
+// let n =arrr.filter((value)=>{
+//     return value % 10 === 0
+// })
+// console.log(n)
+
+// part 4
+
+// let m = [1,2,3,4,56,65,10,20,30];
+// let w =m.map((value)=>{
+//     return value * value
+// })
+// console.log(w)
+
+// part 5
+
+let u = [1,2,3,4,5];
+let p =u.reduce((value1,value2)=>{
+    return value1 * value2
+})
+console.log(p)
+
